524be33658062aca533058d00fdbe310c116b387
oscar-mugambi <oscarmugambi@gmail.com>
2022-04-08 00:53:29 +0300

    added aws upload logic

--- src/components/Uploader.jsx
+++ src/components/Uploader.jsx
@@ -3,19 +3,39 @@ import Dropzone from 'react-dropzone-uploader';
 import 'react-dropzone-uploader/dist/styles.css';
 
 const Uploader = () => {
+  const url = 'https://cfw091dtg9.execute-api.us-east-1.amazonaws.com/default/getPresignedURL2';
+
   const handleChangeStatus = () => {
     console.log('hey');
   };
 
-  const handleSubmit = () => {
-    console.log('hey');
+  // const getUploadParams = () => {
+  //   console.log('here');
+  // };
+
+  const handleSubmit = async (file) => {
+    const target = file[0];
+
+    console.log(file);
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const uploadImage = await fetch(data.uploadURL, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'image/jpeg',
+      },
+      body: target['file'],
+    });
+
+    console.log('uploadImage');
   };
 
   return (
     <Dropzone
+      onSubmit={handleSubmit}
       // getUploadParams={getUploadParams}
       onChangeStatus={handleChangeStatus}
-      onSubmit={handleSubmit}
       styles={{ dropzone: { minHeight: 200, maxHeight: 250 } }}
     />
   );
